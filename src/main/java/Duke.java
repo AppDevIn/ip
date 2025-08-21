155d36d57413169004836ab3e122c075d9d1b2a4
Jeya <e1356515@u.nus.edu>
2025-08-21 22:03:21 +0800

    rename the bot and implement an initial skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = " _____ ____  ___ _____ _   _ \n"
+                + "|  ___|  _ \\|_ _|_   _| | | |\n"
+                + "| |_  | | | || |  | | | |_| |\n"
+                + "|  _| | |_| || |  | | |  _  |\n"
+                + "|____ |____/|___| |_| |_| |_|\n";
         System.out.println("Hello from\n" + logo);
+
+
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm E.D.I.T.H.");
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }

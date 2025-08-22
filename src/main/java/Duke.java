cd63e8c94b35f1285995992bd18df7e7d227baaf
Jeya <e1356515@u.nus.edu>
2025-08-22 08:27:23 +0800

    Added flow control for mark and unmark

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -28,7 +28,34 @@ public class Duke {
                 printArrayOfItems(listOfItems);
                 System.out.println("____________________________________________________________");
                 continue;
-            } else if (input.equalsIgnoreCase("bye")) {
+            } else if (input.matches("mark \\d+")) {
+                String[] split = input.split(" ");
+                int taskNum = Integer.parseInt(split[1]);
+                if (taskNum >= 1 && taskNum <= index) {
+                    listOfItems[taskNum - 1].markAsDone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" Nice! I've marked this task as done:");
+                    System.out.println("   " + listOfItems[taskNum - 1]);
+                    System.out.println("____________________________________________________________");
+                } else {
+                    printInvalidTaskNumberError();
+                }
+                continue;
+            } else if (input.matches("unmark \\d+")) {
+                String[] parts = input.split(" ");
+                int taskNum = Integer.parseInt(parts[1]);
+                if (taskNum >= 1 && taskNum <= index) {
+                    listOfItems[taskNum - 1].markAsUndone();
+                    System.out.println("____________________________________________________________");
+                    System.out.println(" OK, I've marked this task as not done yet:");
+                    System.out.println("   " + listOfItems[taskNum - 1]);
+                    System.out.println("____________________________________________________________");
+                } else {
+                    printInvalidTaskNumberError();
+                }
+                continue;
+            }
+            else if (input.equalsIgnoreCase("bye")) {
                 break;
             }
             listOfItems[index] = new Task(input);
@@ -46,10 +73,17 @@ public class Duke {
     }
 
     private static void printArrayOfItems(Task[] items) {
+        System.out.println(" Here are the tasks in your list:");
         for (int i = 0; i < items.length; i++) {
             if (items[i] != null) {
-                System.out.println(" " + (i + 1) + ". " + items[i]);
+                System.out.println(" " + (i + 1) + "." + items[i]);
             }
         }
+    }
+    
+    private static void printInvalidTaskNumberError() {
+        System.out.println("____________________________________________________________");
+        System.out.println(" Invalid task number!");
+        System.out.println("____________________________________________________________");
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -21,6 +21,6 @@ public class Task {
 
     @Override
     public String toString() {
-        return getStatusIcon() + " " + getStatusIcon();
+        return "[" + getStatusIcon() + "] " + description;
     }
 }

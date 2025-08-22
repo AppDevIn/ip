733ff3114b2bb0ee9cb03f63ebe6f052a471b955
Jeya <e1356515@u.nus.edu>
2025-08-22 08:20:17 +0800

    Added undone and done

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -11,6 +11,14 @@ public class Task {
         return (isDone ? "X" : " ");
     }
 
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+    }
+
     @Override
     public String toString() {
         return getStatusIcon() + " " + getStatusIcon();

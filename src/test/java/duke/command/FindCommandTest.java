cf3b3d9e5f749e8beece02f947d1ffe1d9c8cf8c
Jeya <e1356515@u.nus.edu>
2025-08-31 00:37:04 +0800

    test: Add test for find command

---
+++ src/test/java/duke/command/FindCommandTest.java
@@ -0,0 +1,97 @@
+package duke.command;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+import duke.storage.Storage;
+import duke.storage.TaskList;
+import duke.task.Todo;
+import duke.task.Deadline;
+import duke.task.Event;
+import duke.task.DateTimeParser;
+import duke.ui.Ui;
+import duke.exception.DukeException;
+
+public class FindCommandTest {
+
+    @Test
+    public void execute_findWithMatchingTasks_displaysCorrectResults() throws DukeException {
+        TaskList tasks = new TaskList();
+        tasks.add(new Todo("read book"));
+        tasks.add(new Todo("buy groceries"));
+        tasks.add(new Deadline("return book", DateTimeParser.parseDateTime("6/6/2023 1800")));
+        tasks.add(new Event("book club meeting",
+                DateTimeParser.parseDateTime("10/6/2023 1900"),
+                DateTimeParser.parseDateTime("10/6/2023 2100")));
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        FindCommand findCommand = new FindCommand("find book");
+        Ui ui = new Ui();
+        Storage storage = new Storage("data", "test.txt");
+
+        findCommand.execute(tasks, ui, storage);
+
+        System.setOut(originalOut);
+        String output = outputStream.toString();
+
+        assertTrue(output.contains("Here are the matching tasks in your list:"));
+        assertTrue(output.contains("1.[T][ ] read book"));
+        assertTrue(output.contains("3.[D][ ] return book"));
+        assertTrue(output.contains("4.[E][ ] book club meeting"));
+    }
+
+    @Test
+    public void execute_findWithNoMatches_displaysNoResults() throws DukeException {
+        TaskList tasks = new TaskList();
+        tasks.add(new Todo("buy groceries"));
+        tasks.add(new Todo("do homework"));
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        FindCommand findCommand = new FindCommand("find book");
+        Ui ui = new Ui();
+        Storage storage = new Storage("data", "test.txt");
+
+        findCommand.execute(tasks, ui, storage);
+
+        System.setOut(originalOut);
+        String output = outputStream.toString();
+
+        assertTrue(output.contains("No matching tasks found."));
+    }
+
+    @Test
+    public void execute_caseInsensitiveSearch_findsMatches() throws DukeException {
+        TaskList tasks = new TaskList();
+        tasks.add(new Todo("Read Book"));
+        tasks.add(new Todo("BOOK REVIEW"));
+        tasks.add(new Todo("buy groceries"));
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outputStream));
+
+        FindCommand findCommand = new FindCommand("find book");
+        Ui ui = new Ui();
+        Storage storage = new Storage("data", "test.txt");
+
+        findCommand.execute(tasks, ui, storage);
+
+        System.setOut(originalOut);
+        String output = outputStream.toString();
+
+        assertTrue(output.contains("Here are the matching tasks in your list:"));
+        assertTrue(output.contains("1.[T][ ] Read Book"));
+        assertTrue(output.contains("2.[T][ ] BOOK REVIEW"));
+    }
+}

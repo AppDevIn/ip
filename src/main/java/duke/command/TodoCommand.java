9810828d2bde28e25ab1b5ea6be9a4d82685e71c
Jeya <e1356515@u.nus.edu>
2025-08-30 23:14:39 +0800

    docs: Add JavaDoc for TodoCommand

--- src/main/java/duke/command/TodoCommand.java
+++ src/main/java/duke/command/TodoCommand.java
@@ -12,13 +12,26 @@ import duke.ui.Ui;
 import duke.exception.DukeException;
 
 
+/**
+ * Command for creating a simple todo task.
+ * Takes the user's description and creates a basic task with no time constraints.
+ */
 public class TodoCommand extends Command {
     private String input;
     
+    /**
+     * Creates a todo command from the user's input.
+     * 
+     * @param input the full command string including "todo" and description
+     */
     public TodoCommand(String input) {
         this.input = input;
     }
     
+    /**
+     * Executes the todo command by creating a new Todo task.
+     * Strips off the "todo" part and uses the rest as the description.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws DukeException {
         String description = input.substring(4).trim();
